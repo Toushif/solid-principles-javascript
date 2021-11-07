6784ebec281606ae97e02b524470f0ac1d606d3b
Toushif <toushif.haq@gmail.com>
2021-11-07 23:46:44 +0530

    Open closed principle

---
+++ Open closed principle/example-1.js
@@ -0,0 +1,10 @@
+// Definition :
+// Software entities should be open for extension, but closed for modification.
+
+// Problem statement :
+// Say we need to make class Product which have some proprities of the poduct like its name , color and size. We also need to make a filter to filter these products by size , color , name or combination of the three.
+
+// Abstraction :
+// Make a Product class with the dfined proprities
+// Make a Filter to filter the products by a certain criteria
+
