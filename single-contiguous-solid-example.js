96ddebae3bca92dc7f2bbc9734bb8bef4d46d253
Toushif <toushif.haq@gmail.com>
2021-12-06 08:02:50 +0530

    link added

--- single-contiguous-solid-example.js
+++ single-contiguous-solid-example.js
@@ -1,4 +1,4 @@
-
+// https://medium.com/@cramirez92/s-o-l-i-d-the-first-5-priciples-of-object-oriented-design-with-javascript-790f6ac9b9fa
 // S.O.L.I.D. STANDS FOR:
 // S — Single responsibility principle
 // O — Open closed principle
